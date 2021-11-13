6ebd78dc82ca0ee60af684a4b0e7492298565627
Fernando Marchesano Lerma <k2.fernando.lerma@segurossura.com.br>
2021-11-13 17:53:08 -0300

    Primeiro push para o projeto no github

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# springboot-forum
+***Como gerar o build e executar no docker com uma imagem do projeto
+
+mvn clean package 
+
+docker build -t alura/forum . 
+
+docker run -p 8082:8082 -e SPRING_PROFILES_ACTIVE='dev' -e
+FORUM_DATABASE_URL='jdbc:h2:mem:alura-forum' -e FORUM_DATABASE_USERNAME='sa' -e FORUM_DATABASE_PASSWORD='123456'
+alura/forum
---
+++ src/test/java/br/com/alura/forum/repository/CursoRepositorySemH2Test.java
@@ -0,0 +1,47 @@
+package br.com.alura.forum.repository;
+
+import br.com.alura.forum.modelo.Curso;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+class CursoRepositorySemH2Test {
+
+    @Autowired
+    private CursoRepository repository;
+
+    @Autowired
+    private TestEntityManager em;
+
+    @Test
+    void deveriaCarregarUmCursoAoBuscarPeloNomeTest() {
+        String nomeCurso = "HTML 5";
+
+        Curso html5 = new Curso();
+        html5.setNome(nomeCurso);
+        html5.setCategoria("Programação");
+        em.persist(html5);
+
+        Curso curso = repository.findByNome(nomeCurso);
+        assertNotNull(curso);
+        assertEquals(nomeCurso, curso.getNome());
+    }
+
+    @Test
+    void naoDeveriaCarregarUmCursoCujoNomeNãoEstaCadastradoTest() {
+        String nomeCurso = "JPA";
+        Curso curso = repository.findByNome(nomeCurso);
+        assertNull(curso);
+    }
+}
